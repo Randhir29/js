81685cb392d8f26fc456b173d7f11297a33bfcdf
Randhir29 <randhirveerrajput@gmail.com>
2025-04-07 08:12:03 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton
+//Object.create
+
+//object literals
+const Jsuser= {
+    name:"Randhir",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoggingDays:["Monday","Saturday"]
+}
+console.log(Jsuser.email)
+console.log(Jsuser["email"]);
+
